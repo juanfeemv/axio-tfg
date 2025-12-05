09393b0c808b915b5842a1dc5ab597022320d429
Juanfe <menavegajuanfe2005@gmail.com>
2025-12-05 21:47:04 +0100

    DASDASD

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,17 +1,27 @@
 import { Router } from 'express';
-import { register, login, updateProfile, changePassword, deleteUser } from '../controllers/authController'; 
-import { protect } from '../middlewares/auth'; // Necesito proteger estas rutas
+import { 
+  register, 
+  login, 
+  updateProfile, 
+  changePassword, 
+  deleteUser,
+  forgotPassword,    
+  resetPassword      
+} from '../controllers/authController'; 
+import { protect } from '../middlewares/auth';
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
 
-// Rutas protegidas (necesitan token)
+// RUTAS DE RECUPERACIÓN
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
+
+// Rutas protegidas
 router.put('/profile', protect, updateProfile);
 router.put('/password', protect, changePassword);
-
-// DELETE /api/auth/me -> Borrar mi cuenta 
-router.delete('/me', protect, deleteUser); 
+router.delete('/me', protect, deleteUser);
 
 export default router;
